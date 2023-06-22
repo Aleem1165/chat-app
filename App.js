3d4c86ba8fdd3e1a07a8720ca5159835e9886b76
Aleem1165 <abdulaleem8021@gmail.com>
2023-06-22 21:13:27 +0500

    hackathon

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+// 192.168.0.106
+
 
 export default function App() {
+  const backendURL = "http://192.168.0.106/";
+  useEffect(()=>{
+    (
+      async() =>{
+        try{
+          const {data} = await axios.get("http://192.168.0.106:5000/");
+          console.log("data===>" , data);
+        }catch(error){
+          console.log("error===>" , error);
+        }
+      }
+    )
+    ()
+  },[])
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
