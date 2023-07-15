8dd37859703fc76ac6e9790b225818c1bbe86d2e
Aleem1165 <abdulaleem8021@gmail.com>
2023-07-16 03:58:27 +0500

    chatApp

--- src/config/backendURL.js
+++ src/config/backendURL.js
@@ -1,3 +1,6 @@
-const backendURL = "http://192.168.0.107:6000/"
+// const backendURL = "http://192.168.0.107:6000/"
+const backendURL = "https://chat-app-backend-six-livid.vercel.app/"
+// const backendURL = "https://todo-backend-7pny-3qk3b9c2j-aleem1165.vercel.app/apis/todo/read"
+
 
 export default backendURL;
